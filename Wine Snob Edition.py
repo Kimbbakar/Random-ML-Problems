8a752523144d145602d9eadc7aa51e2b859d9328
Kimbbakar <chowdhuryosman04@gmail.com>
2017-11-13 22:03:18 +0600

    Wine Snob Edition: Split data into train and test sets

--- Wine Snob Edition.py
+++ Wine Snob Edition.py
@@ -13,3 +13,4 @@ data = pd.read_csv('winequality.csv',sep=';')
 Y = data.quality
 X = data.drop('quality',axis = 1)
 
+X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size = 0.2,random_state=123,stratify = Y )
