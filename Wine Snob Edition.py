e53c038a2ef0afbd06728ba0706a71863e47a409
Kimbbakar <chowdhuryosman04@gmail.com>
2017-11-17 07:42:27 +0600

    Fitting the Transformer API

--- Wine Snob Edition.py
+++ Wine Snob Edition.py
@@ -14,3 +14,6 @@ Y = data.quality
 X = data.drop('quality',axis = 1)
 
 X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size = 0.2,random_state=123,stratify = Y )
+
+	
+scaler = preprocessing.StandardScaler().fit(X_train)
